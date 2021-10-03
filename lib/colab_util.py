800cdefe8cc2f8bffecc228cf4983c997ff8c53d
kuangzijian1 <kuangzijian1@hotmail.com>
2021-10-02 20:18:56 -0600

    updated colab_util to support local run

--- lib/colab_util.py
+++ lib/colab_util.py
@@ -1,31 +1,19 @@
-import io
-import os
 import torch
-from skimage.io import imread
 import numpy as np
 import cv2
-from tqdm import tqdm_notebook as tqdm
-import base64
-from IPython.display import HTML
 
 # Util function for loading meshes
 from pytorch3d.io import load_objs_as_meshes
-
-from IPython.display import HTML
-from base64 import b64encode
 
 # Data structures and functions for rendering
 from pytorch3d.structures import Meshes
 from pytorch3d.renderer import (
     look_at_view_transform,
     OpenGLOrthographicCameras, 
-    PointLights, 
-    DirectionalLights, 
-    Materials, 
+    PointLights,
     RasterizationSettings, 
     MeshRenderer, 
-    MeshRasterizer,  
-    SoftPhongShader,
+    MeshRasterizer,
     HardPhongShader,
     TexturesVertex
 )
@@ -83,7 +71,7 @@ def generate_video_from_obj(obj_path, video_path, renderer):
     verts_rgb_colors = get_verts_rgb_colors(obj_path)
     verts_rgb_colors = torch.from_numpy(verts_rgb_colors).to(device)
     textures = TexturesVertex(verts_features=verts_rgb_colors)
-    wo_textures = TexturesVertex(verts_features=torch.ones_like(verts_rgb_colors)*0.75)
+    #wo_textures = TexturesVertex(verts_features=torch.ones_like(verts_rgb_colors)*0.75)
 
     # Load obj
     mesh = load_objs_as_meshes([obj_path], device=device)
@@ -92,23 +80,25 @@ def generate_video_from_obj(obj_path, video_path, renderer):
     vers = mesh._verts_list
     faces = mesh._faces_list
     mesh_w_tex = Meshes(vers, faces, textures)
-    mesh_wo_tex = Meshes(vers, faces, wo_textures)
+    #mesh_wo_tex = Meshes(vers, faces, wo_textures)
 
     # create VideoWriter
     fourcc = cv2. VideoWriter_fourcc(*'MP4V')
-    out = cv2.VideoWriter(video_path, fourcc, 20.0, (1024,512))
+    out = cv2.VideoWriter(video_path, fourcc, 20.0, (512,512))
 
-    for i in tqdm(range(90)):
+    for i in range(1):
         R, T = look_at_view_transform(1.8, 0, i*4, device=device)
         images_w_tex = renderer(mesh_w_tex, R=R, T=T)
         images_w_tex = np.clip(images_w_tex[0, ..., :3].cpu().numpy(), 0.0, 1.0)[:, :, ::-1] * 255
-        images_wo_tex = renderer(mesh_wo_tex, R=R, T=T)
-        images_wo_tex = np.clip(images_wo_tex[0, ..., :3].cpu().numpy(), 0.0, 1.0)[:, :, ::-1] * 255
-        image = np.concatenate([images_w_tex, images_wo_tex], axis=1)
-        out.write(image.astype('uint8'))
+        #images_wo_tex = renderer(mesh_wo_tex, R=R, T=T)
+        #images_wo_tex = np.clip(images_wo_tex[0, ..., :3].cpu().numpy(), 0.0, 1.0)[:, :, ::-1] * 255
+        #image = np.concatenate([images_w_tex, images_wo_tex], axis=1)
+        out.write(images_w_tex.astype('uint8'))
     out.release()
 
-def video(path):
-    mp4 = open(path,'rb').read()
-    data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
-    return HTML('<video width=500 controls loop> <source src="%s" type="video/mp4"></video>' % data_url)
+if __name__ == '__main__':
+    obj_path = '../results/horse_2_test/result_b2_100_0_00.obj'
+    video_path = '../results/horse_2_test/result_b2_100_0_00.mp4'
+    renderer = set_renderer()
+
+    generate_video_from_obj(obj_path, video_path, renderer)
