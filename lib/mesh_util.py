e26f7d8bd95a3cecec1dfe369196262591281c0b
kuangzijian1 <kuangzijian1@hotmail.com>
2021-10-07 13:55:20 -0600

    remove test code

--- lib/mesh_util.py
+++ lib/mesh_util.py
@@ -74,18 +74,6 @@ def save_obj_mesh_with_color(mesh_path, verts, faces, colors):
         file.write('f %d %d %d\n' % (f_plus[0], f_plus[2], f_plus[1]))
     file.close()
 
-def save_obj_mesh_with_color_tester(mesh_path, verts, faces, colors):
-    file = open(mesh_path, 'w')
-
-    for idx, v in enumerate(verts):
-        c = colors[idx]
-        file.write('v %.4f %.4f %.4f %.4f %.4f %.4f\n' % (v[0], v[1], v[2], c[0], c[1], c[2]))
-    for f in faces:
-        f_plus = f + 1
-        file.write('f %d %d %d\n' % (f_plus[0], f_plus[1], f_plus[2]))
-    file.close()
-
-
 def save_obj_mesh_with_uv(mesh_path, verts, faces, uvs):
     file = open(mesh_path, 'w')
 
