74dff7b81c388f009034543d7b3d77790f44d80c
kuangzijian1 <kuangzijian1@hotmail.com>
2021-10-31 19:49:01 -0600

    added multiple gpus device creation

--- lib/net_util.py
+++ lib/net_util.py
@@ -265,7 +265,7 @@ def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[0,1,2]):
         assert (torch.cuda.is_available())
         net.to(gpu_ids[0])
         net = torch.nn.DataParallel(net, gpu_ids)  # multi-GPUs
-        print("Training on multi gpus")
+        print("Training on multi gpus: " + str(gpu_ids))
     init_weights(net, init_type, init_gain=init_gain)
     return net
 
