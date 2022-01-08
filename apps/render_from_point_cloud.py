4c6224cb435cabb51f37f67e92f6c1b5f8274ee4
kuangzijian1 <kuangzijian1@hotmail.com>
2022-01-08 10:10:23 -0700

    re-scale the translation matrix and scale for rendering from point cloud in render_from_point_cloud file

--- apps/render_from_point_cloud.py
+++ apps/render_from_point_cloud.py
@@ -17,43 +17,6 @@ from pytorch3d.renderer import (
     AlphaCompositor
 )
 
-def set_renderer():
-    # Setup
-    device = torch.device("cuda:2")
-    torch.cuda.set_device(device)
-
-    # Initialize a camera.
-    R, T = look_at_view_transform(1, 0, 0)
-    R = torch.tensor([[[-1.],[ 0.],[ 0.]],[[ 0.],[ 1.],[ 0.]],[[ 0.],[ 0.],[-1.]]]).reshape(1,3,3)
-    camera_tensor = torch.load('../results/horse_1_test/stage1_camera_tensor.pt')
-    extrinsic_tensor = torch.load('../results/horse_1_test/stage1_extrinsic_tensor.pt')
-    R_v1 = camera_tensor[..., :, 5:14][0][0].reshape(1, 3, 3)
-    extrinsic_tensor_v1 = extrinsic_tensor[0].unsqueeze(0)
-    T_v1 = extrinsic_tensor_v1[0][0:3, 3].unsqueeze(0) / extrinsic_tensor_v1[0][0:3, 3][1]
-    T[0][0] = -0
-    T[0][1] = -0.5
-    T[0][2] = 100
-    cameras = FoVOrthographicCameras(device=device, R=R_v1, T=T)
-
-    # Define the settings for rasterization and shading. Here we set the output image to be of size
-    # 512x512. As we are rendering images for visualization purposes only we will set faces_per_pixel=1
-    # and blur_radius=0.0. Refer to raster_points.py for explanations of these parameters.
-    raster_settings = PointsRasterizationSettings(
-        image_size=512,
-        radius=0.003,
-        points_per_pixel=10
-    )
-
-    # Create a points renderer by compositing points using an alpha compositor (nearer points
-    # are weighted more heavily). See [1] for an explanation.
-    rasterizer = PointsRasterizer(cameras=cameras, raster_settings=raster_settings)
-    renderer = PointsRenderer(
-        rasterizer=rasterizer,
-        compositor=AlphaCompositor()
-    )
-
-    return renderer
-
 def get_verts_rgb_colors(obj_path):
     rgb_colors = []
 
@@ -79,7 +42,7 @@ def get_ply_rgb_colors(obj_path):
 
     return np.array(rgb_colors, dtype='float32')[None, :, :]
 
-def generate_video_from_obj(obj_path,ply_path, video_path, renderer):
+def generate_video_from_obj(obj_path, ply_path, video_path, renderer, camera_tensor):
     # Setup
     device = torch.device("cuda:2")
     torch.cuda.set_device(device)
@@ -96,16 +59,55 @@ def generate_video_from_obj(obj_path,ply_path, video_path, renderer):
     #verts_list = mesh._verts_list
 
     # Set point cloud object
-    point_cloud = Pointclouds(points=points / 50, features=ply_rgb_colors)
+    scale = 100/camera_tensor[0][0][1]
+    point_cloud = Pointclouds(points=points / scale, features=ply_rgb_colors)
     images_w_tex = renderer(point_cloud)
     images_w_tex = np.clip(images_w_tex[0, ..., :3].cpu().numpy(), 0.0, 1.0)[:, :, ::-1] * 255
     cv2.imwrite(video_path, images_w_tex)
 
+def set_renderer(cameras):
+     # Define the settings for rasterization and shading. Here we set the output image to be of size
+    # 512x512. As we are rendering images for visualization purposes only we will set faces_per_pixel=1
+    # and blur_radius=0.0. Refer to raster_points.py for explanations of these parameters.
+    raster_settings = PointsRasterizationSettings(
+        image_size=512,
+        radius=0.008,
+        points_per_pixel=10
+    )
+
+    # Create a points renderer by compositing points using an alpha compositor (nearer points
+    # are weighted more heavily). See [1] for an explanation.
+    rasterizer = PointsRasterizer(cameras=cameras, raster_settings=raster_settings)
+    renderer = PointsRenderer(
+        rasterizer=rasterizer,
+        compositor=AlphaCompositor()
+    )
+
+    return renderer
 
 if __name__ == '__main__':
-    obj_path = '../results/horse_1_test/stage1_pred_col.obj'
-    ply_path = '../results/horse_1_test/stage1_pred_col.ply'
-    video_path = '../results/horse_1_test/stage1_render_tester.jpg'
-    renderer = set_renderer()
+    obj_path = '../results/bird_2_test/stage1_pred_col.obj'
+    ply_path = '../results/bird_2_test/stage1_pred_col.ply'
+    video_path = '../results/bird_2_test/stage1_render_tester.jpg'
 
-    generate_video_from_obj(obj_path, ply_path, video_path, renderer)
+    # Setup
+    device = torch.device("cuda:2")
+    torch.cuda.set_device(device)
+
+    # Initialize a camera.
+    #R, T = look_at_view_transform(1, 0, 0)
+    camera_tensor = torch.load('../results/bird_2_test/stage1_camera_tensor.pt').to(device)
+    extrinsic_tensor = torch.load('../results/bird_2_test/stage1_extrinsic_tensor.pt').to(device)
+    R_v1 = camera_tensor[..., :, 5:14][0][0].reshape(1, 3, 3)
+    extrinsic_tensor_v1 = extrinsic_tensor[0].unsqueeze(0)
+    T_v1 = extrinsic_tensor_v1[0][0:3, 3].unsqueeze(0) / extrinsic_tensor_v1[0][0:3, 3][1]
+    #T[0][0] = -camera_tensor[0][0][2]/2
+    T_v1[0][0] = -0.03
+    #T_v1[0][1] = -camera_tensor[0][0][3]/(100*0.4*camera_tensor[0][0][1])
+    T_v1[0][1] = -camera_tensor[0][0][3]/52
+    T_v1[0][2] = 100
+    cameras = FoVOrthographicCameras(device=device, R=R_v1, T=T_v1)
+
+    renderer = set_renderer(cameras)
+
+    generate_video_from_obj(obj_path, ply_path, video_path, renderer, camera_tensor)
